4e5e4cc455184173bf65ae3e59480a693007e8f0
pirainogi <pirainogi@gmail.com>
2019-02-27 16:48:57 -0500

    functional sushi, no bonuses

--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,20 +1,23 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 
 const Sushi = (props) => {
+  console.log(props);
+
   return (
     <div className="sushi">
-      <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
-        { 
-          /* Tell me if this sushi has been eaten! */ 
-          true ?
+      <div className="plate">
+        {props.eaten === true ?
             null
           :
-            <img src={/* Give me an image source! */} width="100%" />
+            <img src={props.image} width="100%" alt={props.name} onClick={
+              props.wallet - props.price >= 0
+              ? () => props.eatSushis(props.id, props.price)
+              : null
+            }/>
         }
       </div>
       <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
+        {props.name} - ${props.price}
       </h4>
     </div>
   )
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,31 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
+  console.log(props.wallet);
+  const renderSushis = () => {
+    return props.sushis.map(sushi => {
+      return (
+        <Sushi
+        key={sushi.id}
+        id={sushi.id}
+        name={sushi.name}
+        price={sushi.price}
+        image={sushi.img_url}
+        eaten={sushi.eaten}
+        eatSushis={props.eatSushis}
+        wallet={props.wallet}
+        />
+      )
+    })
+  }
+
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+        {renderSushis()}
+        <MoreButton incrementSushis={props.incrementSushis}/>
       </div>
     </Fragment>
   )
